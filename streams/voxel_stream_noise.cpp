9d418761f92cbadd39d54e2b8aadeb9e11164dca
Den <derofim@yandex.ru>
2020-01-14 03:06:51 +0300

    better VoxelStreamNoise

--- streams/voxel_stream_noise.cpp
+++ streams/voxel_stream_noise.cpp
@@ -1,5 +1,22 @@
 #include "voxel_stream_noise.h"
 
+namespace {
+
+static bool is_ranges_intersect(int start1, int end1, int start2, int end2) {
+	ERR_EXPLAIN("start1 > end1");
+	ERR_FAIL_COND_V(start1 > end1, false);
+
+	ERR_EXPLAIN("start2 > end2");
+	ERR_FAIL_COND_V(start2 > end2, false);
+
+	int max_start = std::max(start1, start2);
+	int min_end = std::min(end1, end2);
+	return (min_end >= max_start);
+}
+
+} // namespace
+
+
 void VoxelStreamNoise::set_channel(VoxelBuffer::ChannelId channel) {
 	ERR_FAIL_INDEX(channel, VoxelBuffer::MAX_CHANNELS);
 	_channel = channel;
@@ -40,80 +57,51 @@ void VoxelStreamNoise::emerge_block(Ref<VoxelBuffer> out_buffer, Vector3i origin
 
 	OpenSimplexNoise &noise = **_noise;
 	VoxelBuffer &buffer = **out_buffer;
+	buffer.clear_channel_f(VoxelBuffer::CHANNEL_SDF, 100.0);
 
-	if (origin_in_voxels.y > _height_start + _height_range) {
+	/// \note iso_scale affects `blockyness` of terrain
+	ERR_EXPLAIN("noise.get_period() <= 0");
+	ERR_FAIL_COND(noise.get_period() <= 0);
+	const float iso_scale = noise.get_period() * 0.1;
+	//print_line(String("iso_scale = {0}").format(varray(iso_scale)));
 
-		if (_channel == VoxelBuffer::CHANNEL_SDF) {
-			buffer.clear_channel_f(_channel, 100.0);
-		}
-		else if (_channel == VoxelBuffer::CHANNEL_TYPE) {
-			buffer.clear_channel(_channel, 0);
-		}
+	float zethaX = origin_in_voxels.x + (buffer.get_size().x << lod);
+	float zethaY = origin_in_voxels.y + (buffer.get_size().y << lod);
+	float zethaZ = origin_in_voxels.z + (buffer.get_size().z << lod);
 
-	} else if (origin_in_voxels.y + (buffer.get_size().y << lod) < _height_start) {
+	/// \note can`t just use height_in_range
+	const bool height_in_range = (buffer.get_size().y - 1) >= 0 && is_ranges_intersect(
+		_height_start,
+		_height_start + _height_range,
+		origin_in_voxels.y, /// \note can not add (0 << lod) because it == 0
+		origin_in_voxels.y + ((buffer.get_size().y - 1) << lod)
+	);
 
-		if (_channel == VoxelBuffer::CHANNEL_SDF) {
-			buffer.clear_channel_f(_channel, -100.0);
-		}
-		else if (_channel == VoxelBuffer::CHANNEL_TYPE) {
-			buffer.clear_channel(_channel, 1);
-		}
+	if (!height_in_range) {
+		return;
+	}
 
-	} else {
+	for (int z = 0; z < buffer.get_size().z; ++z) {
+		for (int x = 0; x < buffer.get_size().x; ++x) {
+			for (int y = 0; y < buffer.get_size().y; ++y) {
+				float lx = origin_in_voxels.x + (x << lod);
+				float ly = origin_in_voxels.y + (y << lod);
+				float lz = origin_in_voxels.z + (z << lod);
 
-		// TODO Proper noise optimization
-		// Prefetching was much faster, but it introduced LOD inconsistencies into the data itself, causing cracks.
-		// Need to implement it properly, or use a different noise library.
-
-		/*FloatBuffer3D &noise_buffer = _noise_buffer;
-		const int noise_buffer_step = 2;
-
-		Vector3i noise_buffer_size = buffer.get_size() / noise_buffer_step + Vector3i(1);
-		if (noise_buffer.get_size() != noise_buffer_size) {
-			noise_buffer.create(noise_buffer_size);
-		}
-
-		// Cache noise at lower grid resolution and interpolate after, much cheaper
-		for (int z = 0; z < noise_buffer.get_size().z; ++z) {
-			for (int x = 0; x < noise_buffer.get_size().x; ++x) {
-				for (int y = 0; y < noise_buffer.get_size().y; ++y) {
-
-					float lx = origin_in_voxels.x + (x << lod) * noise_buffer_step;
-					float ly = origin_in_voxels.y + (y << lod) * noise_buffer_step;
-					float lz = origin_in_voxels.z + (z << lod) * noise_buffer_step;
-
-					float n = noise.get_noise_3d(lx, ly, lz);
-
-					noise_buffer.set(x, y, z, n);
+				/// \note can`t just use height_in_range
+				const bool voxel_in_range = ly < _height_start + _height_range && ly > _height_start;
+				if (!voxel_in_range) {
+					continue;
 				}
-			}
-		}*/
 
-		float iso_scale = noise.get_period() * 0.1;
-		//float noise_buffer_scale = 1.f / static_cast<float>(noise_buffer_step);
+				const float n = noise.get_noise_3d(lx, ly, lz); // ranges from [-1.0 to 1.0]
+				//print_line(String("at {0} n = {1}").format(varray(Vector3(lx, ly, lz), n)));
 
-		Vector3i size = buffer.get_size();
+				/// \note voxel is air if its SDF value is >= 0
+				const float sdf_value = n * iso_scale;
 
-		for (int z = 0; z < size.z; ++z) {
-			for (int x = 0; x < size.x; ++x) {
-				for (int y = 0; y < size.y; ++y) {
-
-					float lx = origin_in_voxels.x + (x << lod);
-					float ly = origin_in_voxels.y + (y << lod);
-					float lz = origin_in_voxels.z + (z << lod);
-
-					//float n = noise_buffer.get_trilinear(x * noise_buffer_scale, y * noise_buffer_scale, z * noise_buffer_scale);
-					float n = noise.get_noise_3d(lx, ly, lz);
-					float t = (ly - _height_start) / _height_range;
-					float d = (n + 2.0 * t - 1.0) * iso_scale;
-
-					if (_channel == VoxelBuffer::CHANNEL_SDF) {
-						buffer.set_voxel_f(d, x, y, z, _channel);
-					}
-					else if (_channel == VoxelBuffer::CHANNEL_TYPE && d < 0) {
-						buffer.set_voxel(1, x, y, z, _channel);
-					}
-				}
+				//print_line(String("at {0} sdf_value = {1}").format(varray(Vector3(lx, ly, lz), sdf_value)));
+				buffer.set_voxel_f(sdf_value, x, y, z, VoxelBuffer::CHANNEL_SDF);
 			}
 		}
 	}
